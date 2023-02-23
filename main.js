5a157844880e9376ffcc0df2002245903cb96305
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-02-23 10:04:54 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function firstNonRepeated(str) {
   for(let i = 0; i < str.length; i++) {
-    if (s.indexOf(str[i]) === str.lastIndexOf(str[i])) {
+    if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
       return str[i]
     }
   }
